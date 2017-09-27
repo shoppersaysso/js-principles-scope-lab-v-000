206f3ed14d3c9a2c580b215db993d5ff31be2b38
Sara Hopper <sara.hopper@gmail.com>
2017-09-27 11:46:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
-var customerName = 'bob'
+var customerName = "bob"
 
-function upperCaseCustomerName(name) {
-  return name.toUpperCase();
+function upperCaseCustomerName() {
+  return customerName.toUpperCase();
 }
