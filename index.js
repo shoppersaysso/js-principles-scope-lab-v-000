4c7e5608f7340351d2c419d0f0a991c68e306dfa
Sara Hopper <sara.hopper@gmail.com>
2017-09-27 11:52:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+const leastFavoriteCustomer = 'sara'
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(newFavorite) {
+  bestCustomer = newFavorite
+}
+
+function changeLeastFavoriteCustomer(newLeast) {
+  leastFavoriteCustomer = newLeast
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = 'kelly'
+  let favoriteCustomer = 'steve'
+}
